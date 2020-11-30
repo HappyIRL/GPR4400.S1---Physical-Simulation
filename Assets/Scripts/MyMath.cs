368af58d4e8740d4fa55a58ddea19002aca6b66a
Julian B <Jlbhappyirl@gmail.com>
2020-11-30 19:34:39 +0100

    Math, PhysicsObject

---
+++ Assets/Scripts/MyMath.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMath : MonoBehaviour
+{
+	//private static MyMath _instance;
+	//private static bool _hasInstance = false;
+	//public static MyMath Instance
+	//{
+	//	get
+	//	{
+	//		if(_instance == null)
+	//		{
+	//			_instance = new MyMath();
+	//			_hasInstance = true;
+	//		}
+	//		return _instance;
+	//	}
+	//	set
+	//	{
+	//		if(_hasInstance)
+	//		{
+	//			return;
+	//		}
+	//		_instance = value;
+	//		_hasInstance = true;
+	//	}
+	//}
+
+
+	public static Vector3 AsUnitVector(Vector3 vector, float scaler)
+	{
+		float xNomralized = vector.x / Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2) + Mathf.Pow(vector.z, 2));
+		float yNormalized = vector.y / Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2) + Mathf.Pow(vector.z, 2));
+		float zNormalized = vector.z / Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2) + Mathf.Pow(vector.z, 2));
+
+		return new Vector3(xNomralized * scaler, yNormalized * scaler, zNormalized * scaler);
+	}
+
+	public static float Magnitude(Vector3 vector)
+	{
+		float magnitude = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2) + Mathf.Pow(vector.z, 2));
+
+		return magnitude;
+	}
+
+
+}
---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	[SerializeField] private Vector3 _velocity = new Vector3(0, -1, 0);
+
+	[SerializeField] private float _mass = 1f;
+	[SerializeField] private float _drag = 0f;
+	[SerializeField] private float _angularDrag = 0f;
+	[SerializeField] private bool _useGravity = true;
+	[SerializeField] private bool _isKinematic = false;
+
+	private float _speed;
+
+	public float Velocity { get; set; }
+
+	public float Acceleration { get; set; }
+
+	private Vector3 _accelerationVector;
+
+	private void Update()
+	{
+
+	}
+
+	private void ApplyVeloctiy()
+	{
+		Vector3 oldPosition = transform.position;
+
+		Vector3 newPosition = oldPosition + _velocity * Time.deltaTime;
+
+		transform.position = newPosition;
+	}
+
+	public void ApplyForce(float force)
+	{
+
+		Acceleration = force / _mass;
+
+		//_velocity = _velocity + Acceleration * Time.deltaTime;
+
+		_speed = MyMath.Magnitude(_velocity);
+
+		transform.position = _velocity;
+	}
+}
